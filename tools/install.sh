1dee596953775320940db181bd2fc695cd960eaa
Andrew Janke <janke@pobox.com>
2015-02-27 23:11:00 -0500

    Add Cygwin support to installer. * Balk at incompatible Windows/MSYS git * Test for chsh presence before trying to use it * Replace non-portable `[[ ... ]]` and `[ x = *pattern* ]` constructs

--- tools/install.sh
+++ tools/install.sh
@@ -1,61 +1,58 @@
 set -e
 
-# Use colors, but only if connected to a terminal, and that terminal
-# supports them.
-ncolors=$(tput colors)
-if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
-  RED="$(tput setaf 1)"
-  GREEN="$(tput setaf 2)"
-  YELLOW="$(tput setaf 3)"
-  BLUE="$(tput setaf 4)"
-  BOLD="$(tput bold)"
-  NORMAL="$(tput sgr0)"
-else
-  RED=""
-  GREEN=""
-  YELLOW=""
-  BLUE=""
-  BOLD=""
-  NORMAL=""
-fi
-
 if [ ! -n "$ZSH" ]; then
   ZSH=~/.oh-my-zsh
 fi
 
 if [ -d "$ZSH" ]; then
-  printf "${YELLOW}You already have Oh My Zsh installed.${NORMAL}\n"
-  printf "You'll need to remove $ZSH if you want to re-install.\n"
+  echo "\033[0;33mYou already have Oh My Zsh installed.\033[0m You'll need to remove $ZSH if you want to install"
   exit
 fi
 
-printf "${BLUE}Cloning Oh My Zsh...${NORMAL}\n"
-hash git >/dev/null 2>&1 && env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH || {
-  printf "git not installed\n"
-  exit
+echo "\033[0;34mCloning Oh My Zsh...\033[0m"
+hash git >/dev/null 2>&1 || {
+  echo "Error: git is not installed"
+  exit 1
+}
+# The Windows (MSYS) Git is not compatible with normal use on cygwin
+if [ "$OSTYPE" = cygwin ]; then
+  if git --version | grep msysgit > /dev/null; then
+    echo "Error: Windows/MSYS Git is not supported on Cygwin"
+    echo "Error: Make sure the Cygwin git package is installed and is first on the path"
+    exit 1
+  fi
+fi
+env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH || {
+  echo "Error: git clone of oh-my-zsh repo failed"
+  exit 1
 }
 
-printf "${BLUE}Looking for an existing zsh config...${NORMAL}\n"
+echo "\033[0;34mLooking for an existing zsh config...\033[0m"
 if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
-  printf "${YELLOW}Found ~/.zshrc.${NORMAL} ${GREEN}Backing up to ~/.zshrc.pre-oh-my-zsh${NORMAL}\n";
+  echo "\033[0;33mFound ~/.zshrc.\033[0m \033[0;32mBacking up to ~/.zshrc.pre-oh-my-zsh\033[0m";
   mv ~/.zshrc ~/.zshrc.pre-oh-my-zsh;
 fi
 
-printf "${BLUE}Using the Oh My Zsh template file and adding it to ~/.zshrc${NORMAL}\n"
+echo "\033[0;34mUsing the Oh My Zsh template file and adding it to ~/.zshrc\033[0m"
 cp $ZSH/templates/zshrc.zsh-template ~/.zshrc
 sed -i -e "/^export ZSH=/ c\\
 export ZSH=$ZSH
 " ~/.zshrc
 
-printf "${BLUE}Copying your current PATH and adding it to the end of ~/.zshrc for you.${NORMAL}\n"
+echo "\033[0;34mCopying your current PATH and adding it to the end of ~/.zshrc for you.\033[0m"
 sed -i -e "/export PATH=/ c\\
 export PATH=\"$PATH\"
 " ~/.zshrc
 
 TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
 if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
-    printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
+  if hash chsh >/dev/null 2>&1; then
+    echo "\033[0;34mTime to change your default shell to zsh!\033[0m"
     chsh -s $(grep /zsh$ /etc/shells | tail -1)
+  else
+    echo "I can't change your shell automatically because this system does not have chsh."
+    echo "Please edit /etc/passwd to set your default shell to zsh."
+  fi
 fi
 unset TEST_CURRENT_SHELL
 
