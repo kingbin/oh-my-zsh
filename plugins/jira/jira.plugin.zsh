ae6d5158de2ade7ab8afd18fc06abb561f551cf6
Marc Cornellà <marc.cornella@live.com>
2015-08-05 00:32:57 +0200

    Use standard open command in current plugins
    Substitutes the current duplicate logic for the standard
    and cross-platform function open_command in plugins:
    
      frontend-search, jira, node, web-search

--- plugins/jira/jira.plugin.zsh
+++ plugins/jira/jira.plugin.zsh
@@ -1,7 +1,6 @@
 # To use: add a .jira-url file in the base of your project
 #         You can also set JIRA_URL in your .zshrc or put .jira-url in your home directory
-#         .jira-url in the current directory takes precedence. The same goes with .jira-prefix
-#         and JIRA_PREFIX.
+#         .jira-url in the current directory takes precedence
 #
 # If you use Rapid Board, set:
 #JIRA_RAPID_BOARD="true"
@@ -12,39 +11,28 @@
 # Usage: jira           # opens a new issue
 #        jira ABC-123   # Opens an existing issue
 open_jira_issue () {
-  local open_cmd
-  if [[ "$OSTYPE" = darwin* ]]; then
-    open_cmd='open'
-  else
-    open_cmd='xdg-open'
-  fi
-
-  local jira_url
   if [ -f .jira-url ]; then
     jira_url=$(cat .jira-url)
   elif [ -f ~/.jira-url ]; then
     jira_url=$(cat ~/.jira-url)
-  elif [[ "${JIRA_URL}" != "" ]]; then
-    jira_url=${JIRA_URL}
+  elif [[ "x$JIRA_URL" != "x" ]]; then
+    jira_url=$JIRA_URL
   else
-    jira_url_help
+    echo "JIRA url is not specified anywhere."
     return 1
   fi
 
-  local jira_prefix
   if [ -f .jira-prefix ]; then
     jira_prefix=$(cat .jira-prefix)
   elif [ -f ~/.jira-prefix ]; then
     jira_prefix=$(cat ~/.jira-prefix)
-  elif [[ "${JIRA_PREFIX}" != "" ]]; then
-    jira_prefix=${JIRA_PREFIX}
   else
     jira_prefix=""
   fi
 
   if [ -z "$1" ]; then
     echo "Opening new issue"
-    $open_cmd "${jira_url}/secure/CreateIssue!default.jspa"
+    open_command "${jira_url}/secure/CreateIssue!default.jspa"
   elif [[ "$1" = "assigned" || "$1" = "reported" ]]; then
     jira_query $@
   else
@@ -55,11 +43,11 @@ open_jira_issue () {
     else
       echo "Opening issue #$1"
     fi
-
-    if [[ "$JIRA_RAPID_BOARD" == "true" ]]; then
-      $open_cmd "${jira_url}/issues/${jira_prefix}${1}${addcomment}"
+    
+    if [[ "x$JIRA_RAPID_BOARD" = "xtrue" ]]; then
+      $open_cmd  "$jira_url/issues/$jira_prefix$1$addcomment"
     else
-      $open_cmd "${jira_url}/browse/${jira_prefix}${1}${addcomment}"
+      $open_cmd  "$jira_url/browse/$jira_prefix$1$addcomment"
     fi
   fi
 }
@@ -88,25 +76,24 @@ jira_name () {
 }
 
 jira_query () {
-  local verb="$1"
-  local jira_name lookup preposition
-  if [[ "${verb}" = "reported" ]]; then
-    lookup=reporter
-    preposition=by
-  elif [[ "${verb}" = "assigned" ]]; then
-    lookup=assignee
-    preposition=to
-  else
-    echo "not a valid lookup $verb"
-    return 1
-  fi
-  shift 1
-  jira_name $@
-  if [[ $? = 1 ]]; then
-    return 1
-  fi
-  echo "Browsing issues ${verb} ${preposition} ${jira_name}"
-  $open_cmd "${jira_url}/secure/IssueNavigator.jspa?reset=true&jqlQuery=${lookup}+%3D+%22${jira_name}%22+AND+resolution+%3D+unresolved+ORDER+BY+priority+DESC%2C+created+ASC"
+    verb="$1"
+    if [[ "${verb}" = "reported" ]]; then
+      lookup=reporter
+      preposition=by
+    elif [[ "${verb}" = "assigned" ]]; then
+      lookup=assignee
+      preposition=to
+    else
+      echo "not a valid lookup $verb"
+      return 1
+    fi
+    shift 1
+    jira_name $@
+    if [[ $? = 1 ]]; then
+        return 1
+    fi
+    echo "Browsing issues ${verb} ${preposition} ${jira_name}"
+    $open_cmd "${jira_url}/secure/IssueNavigator.jspa?reset=true&jqlQuery=${lookup}+%3D+%22${jira_name}%22+AND+resolution+%3D+unresolved+ORDER+BY+priority+DESC%2C+created+ASC"
 }
 
 alias jira='open_jira_issue'
