7e9fb79c420e3d5f30754e684d4cfae94671045f
Andrew Janke <andrew@apjanke.net>
2015-08-17 20:55:41 -0400

    Add defensive `emulate -L zsh` to termsupport functions

--- lib/termsupport.zsh
+++ lib/termsupport.zsh
@@ -8,8 +8,6 @@
 # Limited support for Apple Terminal (Terminal can't set window and tab separately)
 function title {
   emulate -L zsh
-  setopt prompt_subst
-  
   [[ "$EMACS" == *term* ]] && return
 
   # if $2 is unset use $1 as default
@@ -26,13 +24,10 @@ function title {
 
 ZSH_THEME_TERM_TAB_TITLE_IDLE="%15<..<%~%<<" #15 char left truncated PWD
 ZSH_THEME_TERM_TITLE_IDLE="%n@%m: %~"
-# Avoid duplication of directory in terminals with independent dir display
-if [[ $TERM_PROGRAM == Apple_Terminal ]]; then
-  ZSH_THEME_TERM_TITLE_IDLE="%n@%m"
-fi
 
 # Runs before showing the prompt
 function omz_termsupport_precmd {
+  emulate -L zsh
   if [[ $DISABLE_AUTO_TITLE == true ]]; then
     return
   fi
@@ -42,11 +37,11 @@ function omz_termsupport_precmd {
 
 # Runs before executing the command
 function omz_termsupport_preexec {
+  emulate -L zsh
   if [[ $DISABLE_AUTO_TITLE == true ]]; then
     return
   fi
 
-  emulate -L zsh
   setopt extended_glob
 
   # cmd name only, or if this is sudo or ssh, the next cmd
@@ -68,6 +63,7 @@ if [[ "$TERM_PROGRAM" == "Apple_Terminal" ]] && [[ -z "$INSIDE_EMACS" ]]; then
 
   # Emits the control sequence to notify Terminal.app of the cwd
   function update_terminalapp_cwd() {
+    emulate -L zsh
     # Identify the directory using a "file:" scheme URL, including
     # the host name to disambiguate local vs. remote paths.
 
